5307bff74bc5c32b402aca70bd0eca694803f595
joaolgm <joao.menezes@ccc.ufcg.edu.br>
2021-11-29 10:46:55 -0300

    testes

---
+++ src/parser_test.py
@@ -0,0 +1,28 @@
+from parser import parse
+import unittest
+import json
+from google.protobuf.json_format import MessageToDict
+from data import load
+
+
+class TestParser(unittest.TestCase):
+    def test_jan_2020(self):
+        self.maxDiff = None
+        # Json com a saida esperada
+        with open("src/output_test/test_parser/expected_2020.json", "r") as fp:
+            expected_2020 = json.load(fp)
+
+        files = [
+            "src/output_test/test_parser/membros-ativos-contracheque-01-2020.ods",
+            "src/output_test/test_parser/membros-ativos-verbas-indenizatorias-01-2020.ods",
+        ]
+
+        dados = load(files, "2020", "01")
+        result_data = parse(dados, "mpal/01/2020")
+        # Converto o resultado do parser, em dict
+        result_to_dict = MessageToDict(result_data)
+        self.assertEqual(expected_2020, result_to_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
